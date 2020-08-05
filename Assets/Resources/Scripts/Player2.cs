9a573f2e828e9d6cf1b3c8d47bb5d978c15fd461
Suran Basharati <suranbasharati@gmail.com>
2020-08-05 13:56:45 +0200

    Changed the velocity

--- Assets/Resources/Scripts/Player2.cs
+++ Assets/Resources/Scripts/Player2.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player2 : MonoBehaviour
 {
     public float movementSpeed;
-    //float pointer_y = Input.GetAxis("Mouse Y");
+    private Vector2 lastPosition;
+    private Vector2 lastVelocity;
+    private Vector2 lastAcceleration;
 
     public Rigidbody2D rb;
 
@@ -15,60 +17,27 @@ public class Player2 : MonoBehaviour
     }
     private void Update()
     {
-
+        Vector2 position = transform.position;
+        Vector2 velocity = (position - lastPosition) / Time.deltaTime;
+        Vector2 acceleration = (velocity - lastVelocity) / Time.deltaTime;
 
         rb.velocity = new Vector2(0, Input.acceleration.y * movementSpeed);
-        /*
-        Vector2 acc = Input.acceleration;
-        acc.x = 0.0f;
-        rb.AddForce(acc * movementSpeed);
-        */
 
-
-        //Vector3 acc = transform.Translate(0, Input.acceleration.y * movementSpeed * Time.deltaTime, 0);
-
-        //GetComponent<Rigidbody2D>().velocity = Vector2.Lerp(0f, Input.acceleration.y); //transform.Translate(0.0f, Input.acceleration.y * movementSpeed * Time.deltaTime, 0.0f);
-
-        //transform.Translate(0.0f, Input.acceleration.y * movementSpeed * Time.deltaTime, 0.0f);
-
-
-        /*
-                Vector2 movement = new Vector2(Input.acceleration.y, 0.0f);
-
-                GetComponent<Rigidbody2D>().velocity = movement * movementSpeed;
-
-                //GetComponent<Rigidbody2D>().velocity = new Vector2(0, touchPosition.y * movementSpeed);
-
-
-                if (Input.touchCount > 0)
-                {
-
-                    Touch touch = Input.GetTouch(0);
-                    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-                    touchPosition.x = 557f;
-                    touchPosition.z = 0f;
-
-
-
-                    /*
-                   if (pointer_y >= 267f)
-                   {
-                       pointer_y = 267f;
-                   }
-                   else if (pointer_y <= -267f)
-                   {
-                       pointer_y = -267f;
-                   }
-
-
-                //transform.Translate(0, pointer_y, 0 * movementSpeed);
-
-                //transform.position = touchPosition;
-
-
-
-            }
-                */
-
+        if (Mathf.Abs(acceleration.magnitude - lastAcceleration.magnitude) < 0.01f)
+        {
+            // Still
+        }
+        else if (acceleration.magnitude > lastAcceleration.magnitude)
+        {
+            Debug.Log("Accelerating");
+            rb.velocity = new Vector2(0, Input.acceleration.y * movementSpeed);
+            rb.AddForce(transform.up * 1.0f);
+        }
+        else // Decelerating
+        {
+            Debug.Log("Decelerating");
+            rb.velocity = new Vector2(0, Input.acceleration.y * movementSpeed);
+            rb.AddForce(transform.up * -1.0f);
+        }
     }
 }
